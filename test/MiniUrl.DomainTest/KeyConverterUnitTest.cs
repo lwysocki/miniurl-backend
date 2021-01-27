8b0951a20fe9f18f4a81a0d624e9d13cba83db8b
Lukasz Wysocki <wysocki.l@gmail.com>
2021-01-27 15:39:00 +0100

    Key converter key length assert for max value, and value above max

--- test/MiniUrl.DomainTest/KeyConverterUnitTest.cs
+++ test/MiniUrl.DomainTest/KeyConverterUnitTest.cs
@@ -11,21 +11,28 @@ namespace MiniUrl.DomainTest
         public void KeyEncodingShouldReturnsStringOfProperLength()
         {
             int keyLength = 6;
-            long originalValue = 0;
             IKeyConverter keyConverter = new KeyConverter(keyLength);
 
-            string keyMin = keyConverter.Encode(originalValue);
+            long originalMinValue = 0;
+            long originalMaxValue = (long)(Math.Pow(keyConverter.AlphabetSize, keyLength) - Math.Pow(keyConverter.AlphabetSize, keyLength - 1)) - 1;
+            string keyMin = keyConverter.Encode(originalMinValue);
+            string keyMax = keyConverter.Encode(originalMaxValue);
+
+            long exceededMaxValue = originalMaxValue + 1;
+            string keyAboveMax = keyConverter.Encode(exceededMaxValue);
 
             Assert.Equal(keyLength, keyMin.Length);
+            Assert.Equal(keyLength, keyMax.Length);
+            Assert.True(keyAboveMax.Length > keyLength);
         }
 
         [Fact]
         public void KeyDecodingShouldReturnOriginalNumber()
         {
             int keyLength = 6;
-            long originalValue = 0;
             IKeyConverter keyConverter = new KeyConverter(keyLength);
 
+            long originalValue = 0;
             string key = keyConverter.Encode(originalValue);
 
             Assert.Equal(originalValue, keyConverter.Decode(key));
