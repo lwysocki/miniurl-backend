7ce830824fd5f6ec6e421471f0c88eb7a575deab
Lukasz Wysocki <wysocki.l@gmail.com>
2021-01-30 13:21:43 +0100

    Add key generation implementation

---
+++ src/MiniUrl.KeyManager/Domain/KeysGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniUrl.KeyManager.Domain
+{
+    public class KeysGenerator
+    {
+        public long Iteration { get; private set; }
+
+        public KeysGenerator(long iteration = 0)
+        {
+            Iteration = iteration;
+        }
+
+        public IList<long> Generate(long maxValue, long step)
+        {
+            IList<long> keys = new List<long>();
+            long current = ++Iteration;
+
+            while (current < maxValue)
+            {
+                keys.Add(current);
+                current += step;
+            }
+
+            return keys;
+        }
+    }
+}
