8ddebe1c094e0d4660e8c626779c82fa4e9cb250
Lukasz Wysocki <wysocki.l@gmail.com>
2021-02-06 15:06:21 +0100

    Add extension method for JsonDocument to string conversion

---
+++ src/MiniUrl.KeyManager/Utility/JsonDocumentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MiniUrl.KeyManager.Utility
+{
+    public static class JsonDocumentExtensions
+    {
+        public static string ToJsonString(this JsonDocument jsonDocument)
+        {
+            var stream = new MemoryStream();
+            Utf8JsonWriter writer = new Utf8JsonWriter(stream);
+            jsonDocument.WriteTo(writer);
+            writer.Flush();
+
+            return Encoding.UTF8.GetString(stream.ToArray());
+        }
+    }
+}
