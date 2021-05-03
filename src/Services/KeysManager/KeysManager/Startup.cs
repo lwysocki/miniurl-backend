ff95d99e536c6bb6eb10b53df601d720b54b2031
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-03 20:30:15 +0200

    Removed MVC dependencies & configuration from KeysManager

--- src/Services/KeysManager/KeysManager/Startup.cs
+++ src/Services/KeysManager/KeysManager/Startup.cs
@@ -1,22 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using MiniUrl.KeyManager.Services;
-using MiniUrl.KeyManager.Infrastructure;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using MiniUrl.KeyManager.Infrastructure.Repositories;
 using MiniUrl.KeyManager.Domain.Models;
+using MiniUrl.KeyManager.Infrastructure;
+using MiniUrl.KeyManager.Infrastructure.Repositories;
+using MiniUrl.KeyManager.Services;
+using System;
 using System.Reflection;
 
 namespace MiniUrl.KeyManager
@@ -42,10 +35,6 @@ namespace MiniUrl.KeyManager
                     npgsqlOptions.EnableRetryOnFailure(15, TimeSpan.FromSeconds(30), null);
                 });
             });
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniUrl.KeyManager", Version = "v1" });
-            });
 
             services.AddTransient<IKeysManagerRepository, KeysManagerRepository>();
 
@@ -62,19 +51,16 @@ namespace MiniUrl.KeyManager
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MiniUrl.KeyManager v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
             });
         }
     }
