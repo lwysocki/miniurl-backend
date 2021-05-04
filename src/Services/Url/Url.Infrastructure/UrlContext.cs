a0dbc5fb88ed4b021b1a61d72e4f21837ec26516
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-04 19:28:19 +0200

    Create Url db context, with Address entity type configuration

---
+++ src/Services/Url/Url.Infrastructure/UrlContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using MiniUrl.Url.Domain.Model;
+using MiniUrl.Url.Infrastructure.EntityConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniUrl.Url.Infrastructure
+{
+    public class UrlContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+
+        public UrlContext(DbContextOptions<UrlContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AddressEntityTypeConfiguration());
+        }
+    }
+
+    public class KeyManagerDesignFactory : IDesignTimeDbContextFactory<UrlContext>
+    {
+        public UrlContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UrlContext>()
+                .UseNpgsql("Host=localhost;Database=Associations;Username=postgres;Password=postgres");
+
+            return new(optionsBuilder.Options);
+        }
+    }
+}
