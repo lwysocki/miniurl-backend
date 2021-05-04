2ebece2387a89c95e8d7f69a4c70ab507fd95f2b
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-04 19:17:46 +0200

    Create Url.Domain models

---
+++ src/Services/Url/Url.Domain/Model/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniUrl.Url.Domain.Model
+{
+    public class Address
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+
+        public Address(long id, string url)
+        {
+            Id = id;
+            Url = url;
+        }
+    }
+}
