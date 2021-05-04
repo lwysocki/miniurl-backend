c77abd290f78e318df10509c4b54ea48dc7ba7d0
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-04 19:35:46 +0200

    Setup url db context (no migrations)

--- src/Services/Url/Url/Startup.cs
+++ src/Services/Url/Url/Startup.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MiniUrl.Url.Infrastructure;
 using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,11 +17,27 @@ namespace MiniUrl.Url
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            services.AddDbContext<UrlContext>(options =>
+            {
+                options.UseNpgsql(Configuration["ConnectionString"], npgsqlOptions =>
+                {
+                    npgsqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                    npgsqlOptions.EnableRetryOnFailure(15, TimeSpan.FromSeconds(30), null);
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
