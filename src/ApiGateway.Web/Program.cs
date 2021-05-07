f97c3777f9d86bfbb6e8bf14941cf7c90602ebe3
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-07 17:02:34 +0200

    Move gRPC urls to configuration

--- src/ApiGateway.Web/Program.cs
+++ src/ApiGateway.Web/Program.cs
@@ -12,6 +12,16 @@ namespace MiniUrl.ApiGateway.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                {
+                    var sources = builder.Sources;
+                    sources.Insert(3, new Microsoft.Extensions.Configuration.Json.JsonConfigurationSource()
+                    {
+                        Optional = true,
+                        Path = "appsettings.localhost.json",
+                        ReloadOnChange = false
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
