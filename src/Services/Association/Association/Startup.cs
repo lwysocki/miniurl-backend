b5332f5ea4ac8e7d9c4e2499f3576cb2eef600c6
Lukasz Wysocki <wysocki.l@gmail.com>
2021-05-16 12:20:03 +0200

    Add docker, and docker compose support

--- src/Services/Association/Association/Startup.cs
+++ src/Services/Association/Association/Startup.cs
@@ -30,6 +30,8 @@ namespace MiniUrl.Association
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<GrpcUrls>(Configuration.GetSection(GrpcUrls.Section));
+
             services.AddGrpc();
 
             services.AddDbContext<AssociationContext>(options =>
