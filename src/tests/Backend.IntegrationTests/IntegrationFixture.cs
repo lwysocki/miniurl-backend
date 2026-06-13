2b5fb7f7fb0a9e66031c44fca0927922af83abd4
lwysocki <wysocki.l@gmail.com>
2026-06-13 16:59:59 +0200

    refactor: migrate `KeyManager` to integration tests
    - Upgrade EntityFrameworkCore and Npgsql dependencies.
    - Remove `keysmanager-test` service definitions from Docker Compose configurations.
    - Update `KeysManager` program initialization to explicitly read environment variables.
    - Add `KeysManager` project references and WebApplicationFactory setup into Backend.IntegrationTests fixture.

--- src/tests/Backend.IntegrationTests/IntegrationFixture.cs
+++ src/tests/Backend.IntegrationTests/IntegrationFixture.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Xunit;
@@ -10,14 +12,35 @@ namespace MiniUrl.IntegrationTests
 {
     public class IntegrationFixture : IAsyncLifetime
     {
+        public WebApplicationFactory<KeyManager.Startup> KeyManagerFactory { get; private set; }
         public WebApplicationFactory<ApiGateway.Web.Startup> ApiGatewayFactory { get; private set; }
 
+        public HttpClient KeyManagerClient { get; private set; }
         public HttpClient ApiGatewayClient { get; private set; }
 
         public const string url = "google.com";
 
         public async Task InitializeAsync()
         {
+            KeyManagerFactory = new WebApplicationFactory<KeyManager.Startup>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.UseEnvironment("Development");
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        { "ConnectionString", "Host=localhost;Port=5432;Database=KeysManager;Username=postgres;Password=postgres" },
+                        { "GrpcPort", "81" },
+                        { "KeysGenerator:Iteration", "0" },
+                        { "KeysGenerator:Limit", "50000" }, // Down from 67 billion
+                        { "KeysGenerator:Step", "1000" }
+                    });
+                });
+            });
+
+            KeyManagerClient = KeyManagerFactory.CreateClient();
+
             ApiGatewayFactory = new WebApplicationFactory<ApiGateway.Web.Startup>()
             .WithWebHostBuilder(builder =>
                 {
@@ -34,8 +57,10 @@ namespace MiniUrl.IntegrationTests
         public async Task DisposeAsync()
         {
             ApiGatewayClient?.Dispose();
+            KeyManagerClient?.Dispose();
 
             await ApiGatewayFactory.DisposeAsync();
+            await KeyManagerFactory.DisposeAsync();
         }
     }
 }
